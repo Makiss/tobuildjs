4a7156751de24c9dcc3b9c33c4723190f2de836f
Maksym Somushkin <syomushkin.maxim@gmail.com>
2025-10-08 18:31:45 +0200

    feat: mount and destroy vdom

---
+++ packages/runtime/src/attributes.js
@@ -0,0 +1,52 @@
+export function setAttributes(attrs, el) {
+  const { class: className, style, ...otherAttrs } = attrs;
+
+  if (className) {
+    setClass(className, el);
+  }
+
+  if (style) {
+    Object.entries(style).forEach(([prop, value]) => {
+      setStyle(prop, value, el);
+    });
+  }
+
+  for (const [name, value] of Object.entries(otherAttrs)) {
+    setAttribute(name, value, el);
+  }
+}
+
+function setClass(className, el) {
+  el.className = "";
+
+  if (typeof className === "string") {
+    el.className = className;
+  }
+
+  if (Array.isArray(className)) {
+    el.classList.add(...className);
+  }
+}
+
+export function setStyle(name, value, el) {
+  el.style[name] = value;
+}
+
+export function removeStyle(name, el) {
+  el.style[name] = null;
+}
+
+export function setAttribute(name, value, el) {
+  if (value == null) {
+    removeAttribute(name, el);
+  } else if (name.startsWith("data-")) {
+    el.setAttribute(name, value);
+  } else {
+    el[name] = value;
+  }
+}
+
+export function removeAttribute(name, el) {
+  el[name] = null;
+  el.removeAttribute(name);
+}
---
+++ packages/runtime/src/events.js
@@ -0,0 +1,22 @@
+export function addEventListener(eventName, handler, el) {
+  el.addEventListener(eventName, handler);
+
+  return handler;
+}
+
+export function addEventListeners(listeners = {}, el) {
+  const addedListeners = {};
+
+  Object.entries(listeners).forEach(([eventName, handler]) => {
+    const listener = addEventListener(eventName, handler);
+    addedListeners[eventName] = listener;
+  });
+
+  return addedListeners;
+}
+
+export function removeEventListeners(listeners = {}, el) {
+  Object.entries(listeners).forEach(([eventName, handler]) => {
+    el.removeEventListeners(eventName, handler);
+  });
+}
